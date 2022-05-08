166ba1dd80917022c857f25f6c6fe0a6f0e20c4b
Ambili das v s <ambili.kan@gmail.com>
2022-05-08 10:41:57 +0530

    Program for computing quotient and remainder is added

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace QuotientAndReminder
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter divident: ");
+            int divident=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter divisor: ");
+            int divisor=Convert.ToInt32(Console.ReadLine());
+
+            float quotient=(float)divident/divisor;
+            int reminder=divident%divisor;
+            Console.WriteLine("Quotient = " + quotient);
+            Console.WriteLine("Remainder = "+ reminder);
+
+        }
+    }
+}
